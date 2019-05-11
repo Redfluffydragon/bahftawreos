fef061e90bab1c5d63e79cf1fe0f3ddf2b1be521
Redfluffydragon <kderu1420@gmail.com>
2019-05-10 21:09:02 -0600

    Redirect try five

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if (window.location !== window.parent.location) {
-  window.location.href = 'https://Redfluffydragon.github.io/bahftawreos/';
-  /* let redirect = document.createElement('a');
+  // window.top.location.href = 'https://Redfluffydragon.github.io/bahftawreos/';
+  let redirect = document.createElement('a');
   redirect.href = 'https://Redfluffydragon.github.io/bahftawreos/';
   document.body.appendChild(redirect);
-  redirect.click(); */
+  // redirect.click();
 }
