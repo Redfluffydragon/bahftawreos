7b3c39102e6b7816658c8edcf78528ecc23e2b13
Redfluffydragon <kderu1420@gmail.com>
2019-05-10 21:02:35 -0600

    Redirect try two

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 if (!parent.document.location) {
-  window.location.href = 'https://Redfluffydragon.github.io/bahftawreos/';
+  // window.location.href = 'https://Redfluffydragon.github.io/bahftawreos/';
+  let redirect = document.createElement('a');
+  redirect.href = 'https://Redfluffydragon.github.io/bahftawreos/';
+  document.body.appendChild(redirect);
+  redirect.click();
   console.log('not canon');
 }
