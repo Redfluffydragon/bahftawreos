18572cdf1db0550122bce5eb1aab57c37fd5c635
Redfluffydragon <kderu1420@gmail.com>
2019-05-13 18:19:17 -0600

    Auto redirect

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-if (window.location !== window.parent.location) {
-  // window.top.location.href = 'https://Redfluffydragon.github.io/bahftawreos/';
-  //get around too many redirects somehow
-  /* let redirect = document.createElement('a');
-  redirect.href = 'https://Redfluffydragon.github.io/bahftawreos/';
-  redirect.textContent = 'Redirect';
-  document.body.appendChild(redirect);
-  redirect.click(); */
-}
-
 const elements = {
   'Ba': {name: 'Barium', symbol: 'Ba', number: 56, group: 'Alkaline Earth Metal'},
   'Hf': {name: 'Hafnium', symbol: 'Hf', number: 72, group: 'Transition Metal'},
@@ -17,12 +7,9 @@ const elements = {
   'Os': {name: 'Osmium', symbol: 'Os', number: 76, group: 'Transition Metal'}
 }
 
-let nameTable = document.getElementById('nameTable');
-
-nameTable.addEventListener('click', e => {
+document.getElementById('nameTable').addEventListener('click', e => {
   if (e.target.cellIndex !== undefined) {
     let element = elements[e.target.textContent];
-    console.log(element);
     document.getElementById('name').textContent = element.name;
     document.getElementById('info').textContent = element.group;
     document.getElementById('number').textContent = element.number;
